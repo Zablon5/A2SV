9082a6f4a3dfb962381fc73bf652d0d6da89798f
Zerihun Girma <zgirma74@gmail.com>
2023-02-07 16:08:46 +0300

    Create LeetCode-876.py

---
+++ FEB-week1/LeetCode-876.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        h=head
+        t=head
+        while h and h.next:
+            t=t.next
+            h=h.next.next
+        return t    
+
+        
