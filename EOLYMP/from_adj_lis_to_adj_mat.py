1e7bdd896b644f26eecae4c0c3b8fc9bea932bd3
Zerihun Girma <zgirma74@gmail.com>
2023-05-23 15:30:17 +0300

    Create from_adj_lis_to_adj_mat.py

---
+++ EOLYMP/from_adj_lis_to_adj_mat.py
@@ -0,0 +1,10 @@
+n=int(input())
+mat=[[0 for i in range(n)]for j in range(n)]
+inp=[]
+for i in range(n):
+    adj=list(map(int,input().split()))
+    inp.append(adj)
+for i in range(n):
+    for j in range(inp[i][0]):
+        mat[i][inp[i][j+1]-1]=1
+    print(*mat[i])
