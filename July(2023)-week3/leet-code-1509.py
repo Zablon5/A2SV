7cbcce2b3f2cfe86aef63634c5060300e3051499
Zerihun Girma <zgirma74@gmail.com>
2023-07-16 12:09:38 +0300

    Create leet-code-1509.py

---
+++ July(2023)-week3/leet-code-1509.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minDifference(self, nums: List[int]) -> int:
+        nums=sorted(nums)
+        if len(nums)<=4:
+            return 0
+        w=nums[-4]-nums[0]    
+        x=nums[-1]-nums[3]  
+        y=nums[-2]-nums[2]
+        z=nums[-3]-nums[1]
+
+        return min(w,x,y,z)  
