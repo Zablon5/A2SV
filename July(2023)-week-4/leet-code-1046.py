3923e286211bde4172c521f20c28ec6476ea7312
Zerihun Girma <zgirma74@gmail.com>
2023-07-27 11:01:26 +0300

    Create leet-code-1046.py

---
+++ July(2023)-week-4/leet-code-1046.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        heap=[-i for i in stones]
+        heapq.heapify(heap)
+        while len(heap)>1:
+            y,x=heapq.heappop(heap),heapq.heappop(heap)
+            if x==y:
+                continue
+            else:
+                heapq.heappush(heap,y-x)  
+            print(heap)     
+        if len(heap)==0:
+            return 0      
+        return -heap[0]    
