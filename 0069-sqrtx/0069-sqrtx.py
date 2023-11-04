3c240b128a08e04d201e6a8ba209b95060ec5247
Zerihun Girma <zgirma74@gmail.com>
2023-11-05 00:09:29 +0300

    Time: 41 ms (74.1%), Space: 16.1 MB (66.33%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x==0 or x==1:
+            return x
+        l=1
+        r=x
+        ans=1
+        while l<=r:
+            mid=(l+r)//2
+            if mid*mid<=x:
+                ans=mid
+                l=mid+1
+
+            elif mid*mid>x:
+                r=mid-1
+
+        return ans
+
+        
