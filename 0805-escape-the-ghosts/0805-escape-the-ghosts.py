42cb9b8f2efd9dc114abc9db2f047d23dbcda88e
Zerihun Girma <zgirma74@gmail.com>
2025-02-03 09:31:48 +0300

    Time: 0 ms (100%), Space: 17.8 MB (50.54%) - LeetHub

---
+++ 0805-escape-the-ghosts/0805-escape-the-ghosts.py
@@ -0,0 +1,8 @@
+class Solution:
+    def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
+        dist = abs(target[0]) + abs(target[1])
+
+        for x, y in ghosts:
+            if abs(x - target[0]) + abs(y - target[1]) <= dist:
+                return False
+        return True
